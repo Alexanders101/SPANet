f484bd73c8f85530b6c0fc13e1514e978d7ea796
Javier Duarte <jduarte@ucsd.edu>
2022-10-29 08:23:12 -0700

    v2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="spanet",
     packages=find_packages(),
-    version="0.1.0",
+    version="2.0.0",
     description="spanet",
     author="Alex Shmakov",
     license="BSD-3-Clause"
