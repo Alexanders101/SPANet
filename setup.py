a7cecab61ea090505ffa02c33293fe3e0dfa1489
Alex Shmakov <Alexanders101@gmail.com>
2023-03-01 09:28:52 -0800

    Update pip package.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name="spanet",
     packages=find_packages(),
-    version="2.0.1",
+    version="2.1.0",
     description="Symmetry Preserving Attention Networks",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/Alexanders101/SPANet",
     license="BSD-3-Clause",
     install_requires=[
-        'torch>=1.10',
+        'torch>=1.12',
         'numpy>=1.22',
         'h5py>=3.7',
         'numba>=0.55',
@@ -32,6 +32,7 @@ setup(
         'Environment :: Console',
         'Environment :: GPU :: NVIDIA CUDA :: 11.3',
         'Environment :: GPU :: NVIDIA CUDA :: 11.6',
+        'Environment :: GPU :: NVIDIA CUDA :: 11.7',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
