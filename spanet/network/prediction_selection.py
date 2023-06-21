1ca94949151753af5b567d502169603c602816b4
ereinha <74273926+ereinha@users.noreply.github.com>
2023-06-21 14:54:58 -0500

    Update prediction_selection.py
    Updated with Parton-Iteration combinatorics -- Eric Reinhardt

--- spanet/network/prediction_selection.py
+++ spanet/network/prediction_selection.py
@@ -120,11 +120,11 @@ def mask_jet(data, num_partons, max_jets, index, value):
     #     mask_8(data, max_jets, index, value)
 
 
-@njit("int64[::1](int64, int64)")
-def compute_strides(num_partons, max_jets):
+@njit("int64[::1](int64, int64, int64)")
+def compute_strides(num_partons, max_jets, start_index):
     strides = np.zeros(num_partons, dtype=np.int64)
     strides[-1] = 1
-    for i in range(num_partons - 2, -1, -1):
+    for i in range(num_partons - 2, start_index, -1):
         strides[i] = strides[i + 1] * max_jets
 
     return strides
@@ -165,8 +165,8 @@ def maximal_prediction(predictions):
     return best_jet, best_prediction, best_value
 
 
-@njit(TResult(TPrediction, TInt64[::1], TInt64))
-def extract_prediction(predictions, num_partons, max_jets):
+@njit(TResult(TPrediction, TInt64[::1], TInt64, TInt64))
+def extract_prediction(predictions, num_partons, max_jets, stride_start_index):
     float_negative_inf = -np.float32(np.inf)
     max_partons = num_partons.max()
     num_targets = len(predictions)
@@ -174,42 +174,39 @@ def extract_prediction(predictions, num_partons, max_jets):
     # Create copies of predictions for safety and calculate the output shapes
     strides = []
     for i in range(num_targets):
-        strides.append(compute_strides(num_partons[i], max_jets))
+        strides.append(compute_strides(num_partons[i], max_jets, stride_start_index))
 
-    # Fill up the prediction matrix
-    # -2 : Not yet assigned
-    # -1 : Masked value
-    # else : The actual index value
-    results = np.zeros((num_targets, max_partons), np.int64) - 2
+    # Initialize total_results array
+    total_results = np.zeros((num_targets, max_partons, max_partons), np.int64)
 
     for _ in range(num_targets):
         best_jet, best_prediction, best_value = maximal_prediction(predictions)
 
         if not np.isfinite(best_value):
-            return results
+            return total_results
 
         best_jets = unravel_index(best_jet, strides[best_prediction])
 
-        results[best_prediction, :] = -1
+        total_results[best_prediction, :, stride_start_index] = -1
         for i in range(num_partons[best_prediction]):
-            results[best_prediction, i] = best_jets[i]
+            total_results[best_prediction, i, stride_start_index] = best_jets[i]
 
         predictions[best_prediction][:] = float_negative_inf
         for i in range(num_targets):
             for jet in best_jets:
                 mask_jet(predictions[i], num_partons[i], max_jets, jet, float_negative_inf)
 
-    return results
+    return total_results
 
 
-@njit(TResults(TPredictions, TInt64[::1], TInt64, TInt64), parallel=True)
-def _extract_predictions(predictions, num_partons, max_jets, batch_size):
+@njit(TResults(TPredictions, TInt64[::1], TInt64, TInt64, TInt64), parallel=True)
+def _extract_predictions(predictions, num_partons, max_jets, batch_size, stride_start_index):
     output = np.zeros((batch_size, len(predictions), num_partons.max()), np.int64)
     predictions = [p.copy() for p in predictions]
 
     for batch in numba.prange(batch_size):
         current_prediction = numba.typed.List([prediction[batch] for prediction in predictions])
-        output[batch, :, :] = extract_prediction(current_prediction, num_partons, max_jets)
+        output[batch, :, :, stride_start_index] = extract_prediction(current_prediction, num_partons, max_jets, stride_start_index)
 
     return np.ascontiguousarray(output.transpose((1, 0, 2)))
 
@@ -218,7 +215,17 @@ def extract_predictions(predictions: List[TArray]):
     flat_predictions = numba.typed.List([p.reshape((p.shape[0], -1)) for p in predictions])
     num_partons = np.array([len(p.shape) - 1 for p in predictions])
     max_jets = max(max(p.shape[1:]) for p in predictions)
+    max_partons = max(num_partons)
     batch_size = max(p.shape[0] for p in predictions)
 
-    results = _extract_predictions(flat_predictions, num_partons, max_jets, batch_size)
-    return [result[:, :partons] for result, partons in zip(results, num_partons)]
+    total_results = np.zeros((len(predictions), batch_size, max_partons, max_partons), dtype=np.int64)
+
+    for stride_start_index in range(max_partons):
+        results = _extract_predictions(flat_predictions, num_partons, max_jets, batch_size, stride_start_index)
+        total_results[:, :, :, stride_start_index] = results
+
+    # Select the subset of total_results with the maximum product along the last axis
+    max_product_indices = np.argmax(np.prod(total_results, axis=-1), axis=-1)
+    selected_results = np.take_along_axis(total_results, max_product_indices[..., None], axis=-1)
+
+    return [result[:, :partons, 0] for result, partons in zip(selected_results, num_partons)]
