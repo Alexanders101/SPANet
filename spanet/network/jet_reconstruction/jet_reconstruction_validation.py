cbb52c221d61f6a7a2ea70a99256c4f38bdbf712
Billy Li <li000400@umn.edu>
2024-07-16 22:01:42 -0700

    dev: weighted average of target accuracy

--- spanet/network/jet_reconstruction/jet_reconstruction_validation.py
+++ spanet/network/jet_reconstruction/jet_reconstruction_validation.py
@@ -37,7 +37,7 @@ class JetReconstructionValidation(JetReconstructionNetwork):
             # "average_precision": sk_metrics.average_precision_score
         }
 
-    def compute_metrics(self, jet_predictions, particle_scores, stacked_targets, stacked_masks):
+    def compute_metrics(self, jet_predictions, particle_scores, stacked_targets, stacked_masks, stacked_weights):
         event_permutation_group = self.event_permutation_tensor.cpu().numpy()
         num_permutations = len(event_permutation_group)
         num_targets, batch_size = stacked_masks.shape
@@ -47,14 +47,17 @@ class JetReconstructionValidation(JetReconstructionNetwork):
         # First compute raw_old accuracy so that we can get an accuracy score for each event
         # This will also act as the method for choosing the best permutation to compare for the other metrics.
         jet_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=bool)
+        weighted_jet_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=bool)
         particle_accuracies = np.zeros((num_permutations, num_targets, batch_size), dtype=bool)
         for i, permutation in enumerate(event_permutation_group):
-            for j, (prediction, target) in enumerate(zip(jet_predictions, stacked_targets[permutation])):
+            for j, (prediction, target, weight) in enumerate(zip(jet_predictions, stacked_targets[permutation], stacked_weights[permutation])):
                 jet_accuracies[i, j] = np.all(prediction == target, axis=1)
+                weighted_jet_accuracies[i, j] = np.all(prediction == target, axis=1) * weight
 
             particle_accuracies[i] = stacked_masks[permutation] == particle_predictions
 
         jet_accuracies = jet_accuracies.sum(1)
+        weighted_jet_accuracies = weighted_jet_accuracies.sum(1)
         particle_accuracies = particle_accuracies.sum(1)
 
         # Select the primary permutation which we will use for all other metrics.
@@ -64,7 +67,9 @@ class JetReconstructionValidation(JetReconstructionNetwork):
 
         # Compute final accuracy vectors for output
         num_particles = stacked_masks.sum(0)
+        tot_target_weights = (stacked_masks * stacked_weights).sum(0)
         jet_accuracies = jet_accuracies.max(0)
+        weighted_jet_accuracies = weighted_jet_accuracies.max(0)
         particle_accuracies = particle_accuracies.max(0)
 
         # Create the logging dictionaries
@@ -93,18 +98,8 @@ class JetReconstructionValidation(JetReconstructionNetwork):
         # early stopping, hyperparameter optimization, learning rate scheduling, etc.
         metrics["validation_accuracy"] = metrics[f"jet/accuracy_{num_targets}_of_{num_targets}"]
 
-
-        jet_full_target_accuracies = np.zeros((batch_size), dtype=float)
-        for i in range(1, num_targets + 1):
-            has_i_target = num_particles == i
-            jet_full_target_accuracies[has_i_target] = jet_accuracies[has_i_target] / i
-
-        weights = np.ones_like(jet_full_target_accuracies)
-        if self.balance_particles:
-            class_indices = (stacked_masks * self.particle_index_tensor_np[..., np.newaxis]).sum(0)
-            weights *= self.particle_weights_tensor_np[class_indices]
-
-        metrics["validation_average_jet_accuracy"] = (jet_full_target_accuracies * weights).sum() / weights.sum()
+        weighted_avg_jet_accuracy = weighted_jet_accuracies / tot_target_weights
+        metrics["validation_average_jet_accuracy"] = weighted_avg_jet_accuracy
 
         return metrics
 
@@ -119,9 +114,11 @@ class JetReconstructionValidation(JetReconstructionNetwork):
         # Stack all of the targets into single array, we will also move to numpy for easier the numba computations.
         stacked_targets = np.zeros(num_targets, dtype=object)
         stacked_masks = np.zeros((num_targets, batch_size), dtype=bool)
-        for i, (target, mask) in enumerate(targets):
+        stacked_weights = np.zeros((num_targets, batch_size), dtype=float)
+        for i, (target, mask, weight) in enumerate(targets):
             stacked_targets[i] = target.detach().cpu().numpy()
             stacked_masks[i] = mask.detach().cpu().numpy()
+            stacked_weights[i] = weight.detach().cpu().numpy()
 
         regression_targets = {
             key: value.detach().cpu().numpy()
@@ -142,7 +139,7 @@ class JetReconstructionValidation(JetReconstructionNetwork):
                     prediction[:, indices] = np.sort(prediction[:, indices])
                     target[:, indices] = np.sort(target[:, indices])
 
-        metrics.update(self.compute_metrics(jet_predictions, particle_scores, stacked_targets, stacked_masks))
+        metrics.update(self.compute_metrics(jet_predictions, particle_scores, stacked_targets, stacked_masks, stacked_weights))
 
         for key in regressions:
             delta = regressions[key] - regression_targets[key]
